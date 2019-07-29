67257fb742155d7758060b081e7bc51647697e89
vlopez07 <victor.lopez07@gmail.com>
2019-07-29 07:40:51 -0400

    Trabajando con las props

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*
+const element = document.createElement("h1")
+const contenedor = document.getElementById("root")
+element.innerText="Hola Victor Alexander"
+contenedor.appendChild(element)
+*/
+import React from "react"
+import ReactDOM from "react-dom"
+import Card from "./components/Card"
+ 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const contenedor = document.getElementById("root")
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(<Card
+                    title="Guia Tecnica"
+                    descripcion="Aprenda a diseña increibles imagenes"
+                    img=""
+                    leftColor="#A74CF2"
+                    rightColor="#617BFB"
+                />,contenedor)
+
+
+
